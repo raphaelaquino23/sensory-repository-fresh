d7ed0cec10d03646b6c88e88412592a7ae30c0a0
carylvallejo <carylchrisabelle.vallejo@benilde.edu.ph>
2023-03-05 22:33:09 +0800

    Tested UserList

--- sensory-frontend/src/components/UserManagement/UserList.tsx
+++ sensory-frontend/src/components/UserManagement/UserList.tsx
@@ -93,14 +93,14 @@ const UserList = () => {
         </thead>
         <tbody>
           {listUserInformation
-            .filter((user: { UserInformation_Name: string, UserInformation_Email: string}) => {
+            .filter((user: { UserInformation_Name: string, UserInformation_Email: string, UserType_Name: string}) => {
               if(user) {
                 if (search === "") {
                   return user;
                 } else if (
                   user.UserInformation_Name.toLowerCase().includes(search.toLowerCase()) ||
-                  user.UserInformation_Email.toLowerCase().includes(search.toLowerCase())
-                  // user.UserType_Name.toLowerCase().includes(search.toLowerCase()) 
+                  user.UserInformation_Email.toLowerCase().includes(search.toLowerCase()) ||
+                  user.UserType_Name.toLowerCase().includes(search.toLowerCase()) 
                 ) {
                   return user;
                 }
@@ -111,6 +111,7 @@ const UserList = () => {
                 user: { UserInformation_Id: Key | null | undefined }
               ) => (
                 <tr key={user.UserInformation_Id}>
+                  {/* <tr key={user.UserType_Id}></tr> */}
                   <User user={user} />
                 </tr>
               )
@@ -130,3 +131,152 @@ const UserList = () => {
 }
 
 export default UserList;
+
+// import { Modal, Button, Alert} from 'react-bootstrap';
+// import { Key, useContext, useEffect, useState } from 'react';
+// import {UserContext} from '../../contexts/UserContext';
+// import User from './User';
+// import UserRegistrationPage from '../../pages/UserRegistrationPage'; //Registration
+// // import UserPagination from './UserPagination';
+// import axios from 'axios';
+// import { axiosPrivate } from '../../api/axios';
+// import '../../index.css';
+
+// const UserList = () => {
+//   // const {sortedUsers} = useContext(UserContext);
+
+//   const [listUserInformation, setListUserInformation] = useState([]);
+//   // const [listUser, setListUser] = useState([]);
+//   // const [listUserType, setListUserType] = useState([]);
+//   // const [userManagement, setUserManagement] = useState([]);
+
+
+//   // const [showAlert, setShowAlert] = useState(false);
+//   const [search, setSearch] = useState('');
+//   // const [show, setShow] = useState(false);
+//   // const [currentPage, setCurrentPage] = useState(1);
+//   // const [usersPerPage] = useState(10)
+
+//   // const handleShow = () => setShow(true);
+//   // const handleClose = () => setShow(false);
+
+//   const getUserData = () => {
+//     let endpoints = [
+//       'http://localhost:3081/api/userinformation',
+//       'http://localhost:3081/api/usertype',
+//       'http://localhost:3081/api/user'
+//     ];
+//     Promise.all(endpoints.map((endpoint) => axiosPrivate.get(endpoint))).then(([{data: listUserInformation}]) => {
+//       setListUserInformation(listUserInformation)
+//     });
+//   }
+
+//   useEffect(() => {
+//     getUserData();
+//   }, []);
+  
+//   const handleSearchUSer = (event: React.ChangeEvent<any>) => {
+//     setSearch(event.target.value)
+//   }
+
+//   // useEffect(() => {
+//   //   axiosPrivate.get(`http://localhost:3081/api/userinformation`).then((response) => {
+//   //     setListUserInformation(response.data);
+//   //     console.log("a" + listUserInformation)
+//   //   });
+//   // }, []);
+
+//   // useEffect(() => {
+//   //   axiosPrivate.get(`http://localhost:3081/api/usertype`).then((response) => {
+//   //     setListUserInformation(response.data);
+//   //     console.log("b" + listUserType)
+//   //   });
+//   // }, []);
+
+//   // useEffect(() => {
+//   //   axiosPrivate.get(`http://localhost:3081/api/user`).then((response) => {
+//   //     setListUserInformation(response.data);
+//   //     console.log("c" + listUser)
+//   //   });
+//   // }, []);
+
+
+
+//   // useEffect(() => {
+//   //   axiosPrivate.get(`http://localhost:3081/api/user`).then((response) => {
+//   //     setListUserInformation(response.data);
+//   //   });
+//   // }, []);
+
+//   // const indexOfLastUser = currentPage * usersPerPage;
+//   // const indexOfFirstUser = indexOfLastUser - usersPerPage;
+//   // const currentUsers = sortedUsers.slice(indexOfFirstUser, indexOfLastUser);
+//   // const totalPagesNum = Math.ceil(sortedUsers.length / usersPerPage);
+
+//   return(
+//     <>
+//       <div className="table-title" style={{marginLeft: "1px", marginRight: "3px"}}>
+//         <div className="row">
+//           <div className="col-sm-6">
+//             <h2> User <b>Management </b></h2>
+//           </div>
+//         </div>
+//       </div>
+
+//       <input
+//         className="inpt"
+//         placeholder="Search"
+//         style={{border: "2px solid black"}}
+//         value={search} 
+//         onChange={handleSearchUSer}
+//       />
+
+//       <table className="table table-striped table-hover">
+//         <thead>
+//           <tr>
+//             <th>Username</th>
+//             <th>Email Address</th>
+//             <th>Role</th>
+//             <th>Status</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {listUserInformation
+//             .filter((user: { UserInformation_Name: string, UserInformation_Email: string, UserType_Name: string}) => {
+//               if(user) {
+//                 if (search === "") {
+//                   return user;
+//                 } else if (
+//                   user.UserInformation_Name.toLowerCase().includes(search.toLowerCase()) ||
+//                   user.UserInformation_Email.toLowerCase().includes(search.toLowerCase()) ||
+//                   user.UserType_Name.toLowerCase().includes(search.toLowerCase()) 
+//                 ) {
+//                   return user;
+//                 }
+//               } 
+//             })
+//             .map(
+//               (
+//                 user: { UserInformation_Id: Key | null | undefined }
+//               ) => (
+//                 <tr key={user.UserInformation_Id}>
+//                   {/* <tr key={user.UserType_Id}></tr> */}
+//                   <User user={user} />
+//                 </tr>
+//               )
+//             )}
+//         </tbody>
+//       </table>
+
+//       {/* <UserPagination
+//         pages={totalPagesNum}
+//         setCurrentPage={setCurrentPage}
+//         currentUsers={currentUsers}
+//         sortedUsers={sortedUsers}
+//       /> */}
+//     </>
+//   )
+  
+// }
+
+// export default UserList;
