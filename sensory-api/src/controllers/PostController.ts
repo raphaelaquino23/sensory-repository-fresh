4a8cc7cd3e3099e3c2c04aaa3e201c8a1930dfbb
renzoancajas24 <108566575+renzo-ancajas@users.noreply.github.com>
2023-03-08 21:31:03 +0800

    added loggers in api controllers
    log folder is located in project directory

--- sensory-api/src/controllers/PostController.ts
+++ sensory-api/src/controllers/PostController.ts
@@ -1,4 +1,3 @@
-import { APILogger } from "../logger/api.logger";
 import { winstonLogger } from "../logger/winston.logger";
 import {
   Post,
@@ -10,45 +9,43 @@ import { PostService } from "../service/PostService";
 
 export class PostController {
   private postService: PostService;
-  private logger: APILogger;
 
   constructor() {
     this.postService = new PostService();
-    this.logger = new APILogger();
   }
 
   async getPosts() {
-    this.logger.info("Controller: getPosts", null);
+    winstonLogger.info("Controller: getPosts", null);
     winstonLogger.info("Get all posts method passed.");
     return await this.postService.getPosts();
   }
   async getPostById(Post_Id: number) {
-    this.logger.info("Controller: getPostById", null);
+    winstonLogger.info("Controller: getPostById", null);
     return await this.postService.getPostById(Post_Id);
   }
 
   async getPostInformation() {
-    this.logger.info("Controller: getPostInformation", null);
+    winstonLogger.info("Controller: getPostInformation", null);
     return await this.postService.getPostInformation();
   }
 
   async getPostInformationById(PostInformation_Id: number) {
-    this.logger.info("Controller: getPostInformationById", null);
+    winstonLogger.info("Controller: getPostInformationById", null);
     return await this.postService.getPostInformationById(PostInformation_Id);
   }
 
   async getPostCategory() {
-    this.logger.info("Controller: getPostInformation", null);
+    winstonLogger.info("Controller: getPostInformation", null);
     return await this.postService.getPostCategory();
   }
 
   async getPostStats() {
-    this.logger.info("Controller: getPostInformation", null);
+    winstonLogger.info("Controller: getPostInformation", null);
     return await this.postService.getPostStats();
   }
 
   async getPostStatsById(PostStats_Id: number) {
-    this.logger.info("Controller: getPostStats", null);
+    winstonLogger.info("Controller: getPostStats", null);
     return await this.postService.getPostStatsById(PostStats_Id);
   }
 
@@ -57,7 +54,7 @@ export class PostController {
     postInformation: PostInformation,
     postStats: PostStats
   ) {
-    this.logger.info("Controller: createPost", post);
+    winstonLogger.info("Controller: createPost", post);
     return await this.postService.createPost(post, postInformation, postStats);
   }
 
@@ -65,7 +62,7 @@ export class PostController {
     postinformation: PostInformation,
     poststats: PostStats
   ) {
-    this.logger.info("Controller: createPostInformation", postinformation);
+    winstonLogger.info("Controller: createPostInformation", postinformation);
     return await this.postService.createPostInformation(
       postinformation,
       poststats
@@ -73,12 +70,12 @@ export class PostController {
   }
 
   async createPostCategory(postcategory: PostCategory) {
-    this.logger.info("Controller: createPostCategory", postcategory);
+    winstonLogger.info("Controller: createPostCategory", postcategory);
     return await this.postService.createPostCategory(postcategory);
   }
 
   async createPostStats(poststats: PostStats) {
-    this.logger.info("Controller: createPostStats", poststats);
+    winstonLogger.info("Controller: createPostStats", poststats);
     return await this.postService.createPostStats(poststats);
   }
 
@@ -87,39 +84,39 @@ export class PostController {
     postInformation: PostInformation,
     postStats: PostStats
   ) {
-    this.logger.info("Controller: updatePost", post);
+    winstonLogger.info("Controller: updatePost", post);
     return await this.postService.updatePost(post, postInformation, postStats);
   }
 
   async updatePostInformation(postinformation: PostInformation) {
-    this.logger.info("Controller: updatePostInformation", postinformation);
+    winstonLogger.info("Controller: updatePostInformation", postinformation);
     return await this.postService.updatePostInformation(postinformation);
   }
 
   async updatePostCategory(postcategory: PostCategory) {
-    this.logger.info("Controller: updatePostCategory", postcategory);
+    winstonLogger.info("Controller: updatePostCategory", postcategory);
     return await this.postService.updatePostCategory(postcategory);
   }
 
   async updatePostStats(poststats: PostStats) {
-    this.logger.info("Controller: updatePostStats", poststats);
+    winstonLogger.info("Controller: updatePostStats", poststats);
     return await this.postService.updatePostStats(poststats);
   }
 
   async deletePost(Post_Id: number) {
-    this.logger.info("Controller: deletePost", Post_Id);
+    winstonLogger.info("Controller: deletePost", Post_Id);
     return await this.postService.deletePost(Post_Id);
   }
   async deletePostInformation(PostInformation_Id: number) {
-    this.logger.info("Controller: deletePostInformation", PostInformation_Id);
+    winstonLogger.info("Controller: deletePostInformation", PostInformation_Id);
     return await this.postService.deletePostInformation(PostInformation_Id);
   }
   async deletePostCategory(PostCategory_Id: number) {
-    this.logger.info("Controller: deletePostCategory", PostCategory_Id);
+    winstonLogger.info("Controller: deletePostCategory", PostCategory_Id);
     return await this.postService.deletePostCategory(PostCategory_Id);
   }
   async deletePostStats(PostStats_Id: number) {
-    this.logger.info("Controller: deletePostStats", PostStats_Id);
+    winstonLogger.info("Controller: deletePostStats", PostStats_Id);
     return await this.postService.deletePostStats(PostStats_Id);
   }
   async postUpvoteTracker(Post: Post, User_Id: number) {
