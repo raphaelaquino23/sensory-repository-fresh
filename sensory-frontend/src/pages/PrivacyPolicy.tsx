6e94926dbc8866a6ae3182af5276e52c3b882caf
carylvallejo <carylchrisabelle.vallejo@benilde.edu.ph>
2023-03-12 00:34:06 +0800

    Added terms of service and privacy policy

---
+++ sensory-frontend/src/pages/PrivacyPolicy.tsx
@@ -0,0 +1,53 @@
+const Privacy = () => {
+	return (
+		<div className="container" style={{marginTop:"40px", marginBottom: "40px", marginLeft: "100px", marginRight: "100px", height: "100%"}}>
+			<h1 style={{fontSize:"40px"}}><strong>Privacy Policy</strong></h1>
+			<div>
+			<p>This SensAware Privacy Policy (“Privacy Policy”) applies when you visit our web application.  This Privacy Policy outlines how 
+				SensAware (“we,” “our,” or “us”), may collect and/or use information from you in conjunction with your use of the Site.
+			</p>
+			<p><br />YOU AGREE TO BE BOUND BY THIS PRIVACY POLICY, AND TERMS OF SERVICE (COLLECTIVELY "TERMS OF SERVICE") 
+				BY VISITING OR SUBMITTING REGISTRATION OR LOGIN INFORMATION TO THE SITE. YOUR USE OF THE WEB APPLICATION CONSTITUTES YOUR AGREEMENT 
+				TO THIS PRIVACY STATEMENT, AND CONTINUED USE SIGNIFIES THAT YOU HAVE READ, UNDERSTOOD, AND ACCEPTED THE STATEMENT. DO NOT VISIT OR 
+				LOG IN TO THE SITE IF YOU DO NOT AGREE WITH ANY OF THE STATEMENTS .
+			</p>
+			<p><br /><b>Information collected</b></p>
+			<p style={{marginLeft: "25px"}}><b>Account Information</b></p>
+			<p style={{marginLeft: "45px"}}>In creating an account, SensAware asks some of your personal information such as your username, email address, and a password for 
+				your account. You can also choose to upload your own picture and post a brief description of yourself as a part of your user profile. 
+				Your username will be displayed to all the users of the web application which they can use to send a personal message. If you choose to 
+				use social log in via Facebook, your name and other personal information available will also be collected.
+			</p>
+			<p style={{marginLeft: "45px"}}><br/>When applying for a moderator role, you will be asked for your username, your available schedule, and some questions related to 
+				your experience when moderating for a website. When applying for the role of a therapist, you will be required to declare your 
+				username, your place of employment, your professional license number and the date of validity and the expiration. These information 
+				are asked to be used in validating if you could be given the responsibilities of a therapist when using the web application
+			</p>
+			<p style={{marginLeft: "25px"}}><br/><b>Usage Information</b></p>
+			<p style={{marginLeft: "45px"}}>We collect information about your activity on SenseAware such as:</p>
+			<ul style={{marginLeft: "80px"}}>
+      <li>The articles you upvote</li>
+			<li>The events you checked out and signed up to</li>
+			<li>The contents of the posts you place on the Forum feature</li>
+			<li>Your interactions with the users of the web application such as your comments on their posts and upvotes on the posts you like</li>
+			<li>The username of the users you are sending a private message and the date and time of the message</li>
+			</ul>
+      <p style={{marginLeft: "25px"}}><br/><b>How your information is used</b></p>
+      <ul style={{marginLeft: "55px"}}>
+			<li>All information collected is used to analyze for any possible improvement on the features and functionality of the system and 
+				improve our service so you can have a better experience in using SensAware.</li>
+			<li>Your personal information will be used to verify your identity and prevent any unauthorized usage that may cause harm to the users 
+				of SensAware.</li>
+      </ul>
+			<p style={{marginLeft: "25px"}}><br/><b>How long is your information stored</b></p>
+			<p style={{marginLeft: "45px"}}>Your information is stored on the system as long as you are an active user of SensAware.</p>
+			<p style={{marginLeft: "45px"}}>The data retention period will be six months minimum and five years maximum, depending on retained data. Accounts deemed inactive, which are accounts that have not been logged in for three years, will be made inactive accounts. The user will be notified via email that their account is inactive. Inactive accounts will be deactivated after two more years, giving a total of 5 years before an account is deleted. </p>
+			<p style={{marginLeft: "25px"}}><br/><b>How to contact SensAware</b></p>
+			<p style={{marginLeft: "45px"}}>For any problems, concerns, or inquiries, you may contact SensAware through SensAware@gmail.com. Rest assured that you will receive a response 
+				from your emails within 24 to 48 hours.</p>
+			</div>
+		</div>
+	)
+}
+
+export default Privacy;
